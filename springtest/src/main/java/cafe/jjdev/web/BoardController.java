c044fead588cb2cc6010fb420202f2cd22e583b2
smart-202-10 <smart-202-10@smart-202-10-PC>
2017-10-16 09:52:00 +0900

    BoardMapper.xml 추가 servlet-context.xml에 BoardMapper.xml추가 BoardDao.java추카

--- springtest/src/main/java/cafe/jjdev/web/BoardController.java
+++ springtest/src/main/java/cafe/jjdev/web/BoardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class boardController {
+public class BoardController {
 	 @RequestMapping(value="/boardAdd", method = RequestMethod.GET)
 	    public String boardAdd() {
 	        System.out.println("boardAdd Æû ¿äÃ»");
